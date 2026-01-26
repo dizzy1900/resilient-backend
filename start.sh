b7abb0ba71c201149aa60a587142baa7651c71e2
dizzy1900 <134146888+dizzy1900@users.noreply.github.com>
2026-01-26 18:51:15 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-mkdir -p models
-if [ ! -f models/ag_surrogate.pkl ]; then
-  curl -L -o models/ag_surrogate.pkl \
-    "https://github.com/dizzy1900/adaptmetric-backend/releases/download/v1.0.0/ag.surrogate.pkl"
-fi
+python download_model.py
 
-exec uvicorn app:app --host 0.0.0.0 --port "${PORT:-8000}"
+exec gunicorn main:app --bind 0.0.0.0:${PORT:-8000}
